2cbf8dc36f8a1d1f17b8c7298ac23bb8a05eb33e
Catarina <c.alexandracorreia@ua.pt>
2021-05-05 14:55:23 +0100

    Update test

--- test/test.sh
+++ test/test.sh
@@ -3,9 +3,9 @@
 # Insert stay
 echo -e "Remove data"
 curl -s -d "{\"start_date\": \"2021-04-20\", \"end_date\": \"2021-04-22\", \"stay_id\":\"reservation_id\", \"receipts\": {}}" \
--H "Content-Type: application/json" http://10.0.12.40:8000/stay/delete
+-H "Content-Type: application/json" http://atnog-homeassistant.av.it.pt:8000/stay/clean:8000/stay/delete
 
 
 echo -e "Validate data deletion"
 curl -s -d "{\"start_date\": \"2021-04-20\", \"end_date\": \"2021-04-22\", \"stay_id\":\"reservation_id\", \"receipts\": {}}" \
- -X GET "http://10.0.12.40:8000/stay/clean" | jq .
+ -X GET "http://atnog-homeassistant.av.it.pt:8000/stay/clean:8000/stay/clean" | jq .
