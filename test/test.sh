7937163c2dbac312a12352607c3d1a3122e1cd2d
Catarina <c.alexandracorreia@ua.pt>
2021-05-05 16:34:45 +0100

    Fix hostnames

--- test/test.sh
+++ test/test.sh
@@ -3,16 +3,16 @@
 # Insert stay
 echo -e "Insert stay"
 content=$(curl -s -d "{\"start_date\": \"2021-04-20\", \"end_date\": \"2021-04-22\", \"receipt\": {\"subjectEmail\":\"user@email.com\"}}" \
--H "Content-Type: application/json" http://localhost:8000/stay/add)
+-H "Content-Type: application/json" http://atnog-homeassistant.av.it.pt:8000/stay/add)
 stay_id=$( jq -r  '.stay_id' <<< "${content}" ) 
 echo -e "stay_id= ${stay_id}"
 
 
 # Remove personal data of the renter
 echo -e "Remove personal data of the renter"
-curl -s -X GET "http://localhost:8000/stay/delete?stay_id=$stay_id"
+curl -s -X GET "http://atnog-homeassistant.av.it.pt:8000/stay/delete?stay_id=$stay_id"
 
 
 # Validate data deletion
 echo -e "Validate data deletion"
-curl -s -X GET "http://localhost:8000/stay/clean?stay_id=$stay_id" | jq .
+curl -s -X GET "http://atnog-homeassistant.av.it.pt:8000/stay/clean?stay_id=$stay_id" | jq .
